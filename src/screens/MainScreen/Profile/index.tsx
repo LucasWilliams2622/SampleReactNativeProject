71a245f118236c1d533b1d64f6eb9282580a5397
Lucas_Williams <nguyenvanson2622003@gmail.com>
2024-06-07 21:16:59 +0700

    Revert "test mobxx"
    This reverts commit f2e956283b30c68d23104e457bdb58d4c5d1d7b3.

--- src/screens/MainScreen/Profile/index.tsx
+++ src/screens/MainScreen/Profile/index.tsx
@@ -1,37 +1,16 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React, {useState} from 'react';
+import React from 'react';
 import AppButton from 'src/components/buttons/AppButton';
 import {useNavigation} from '@react-navigation/native';
 import {useAppDispatch, useAppSelector} from 'src/redux/hook';
 import {setIsSignIn} from 'src/redux/slices/authSlice';
-import {observer} from 'mobx-react';
-import {observable, reaction} from 'mobx';
-import {Button} from 'react-native';
-
-// Tạo một biến observable sử dụng MobX
-const appState = observable({
-  count: 0,
-});
-
-// Tạo một phản ứng để hiển thị thông báo khi giá trị count thay đổi
-reaction(
-  () => appState.count,
-  (count, reaction) => {
-    alert(`Giá trị của biến count là: ${count}`);
-  },
-);
 
 const Profile = () => {
   const navigation = useNavigation();
   const isSignIn = useAppSelector(state => state.auth.isSignIn);
   console.log(isSignIn);
   const dispatch = useAppDispatch();
-  const [count, setCount] = useState(0);
 
-  const increaseCount = () => {
-    setCount(count + 1);
-    appState.count = count + 1;
-  };
   const handleLogout = () => {
     dispatch(setIsSignIn(false));
     navigation.navigate('Login');
@@ -50,8 +29,6 @@ const Profile = () => {
           handleLogout();
         }}
       />
-      <Text>Số lần nhấn: {count}</Text>
-      <Button title="Tăng số lần nhấn" onPress={increaseCount} />
     </View>
   );
 };
