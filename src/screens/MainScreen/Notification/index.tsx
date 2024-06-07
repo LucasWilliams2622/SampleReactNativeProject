ae589ac1090418ea1b2c21949c6772e698f467c8
Lucas_Williams <nguyenvanson2622003@gmail.com>
2024-06-07 21:08:40 +0700

    Revert "config: biomatric"
    This reverts commit 1ef51692dd532f7ae6323e7f7efbc87b102e2d99.

--- src/screens/MainScreen/Notification/index.tsx
+++ src/screens/MainScreen/Notification/index.tsx
@@ -1,31 +1,13 @@
-import {
-  StatusBar,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  useColorScheme,
-  View,
-} from 'react-native';
-import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 import AppButton from 'src/components/buttons/AppButton';
 import {useNavigation} from '@react-navigation/native';
 import AppDateTimePicker from 'src/components/picker/AppDatePicker';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import RTNMyBiometric from 'rtn-my-biometric/js/NativeMyBiometric';
-import {SafeAreaView} from 'react-native';
 
 const Notification = () => {
   const navigation = useNavigation();
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const [biometric, setBiometric] = useState('');
-
-  const backgroundStyle = {
-    backgroundColor: !isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView>
+    <View>
       <Text>Notification</Text>
       <AppDateTimePicker />
       <AppButton
@@ -36,35 +18,7 @@ const Notification = () => {
         title="CountDown"
         onPress={() => navigation.navigate('CountDown')}
       />
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <TouchableOpacity
-        onPress={async () => {
-          try {
-            const data = await RTNMyBiometric?.getAvailableBiometric()!;
-            console.log(data);
-            setBiometric(data ?? '');
-          } catch (e) {
-            console.log(e);
-          }
-        }}>
-        <Text>Get available biometricss</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={async () => {
-          try {
-            const isAuthenticated = await RTNMyBiometric?.authenticate();
-            console.log('ISAuthenticated', isAuthenticated);
-          } catch (e) {
-            console.log(e);
-          }
-        }}>
-        <Text>Authenticate Locally using biometric</Text>
-      </TouchableOpacity>
-      <Text>Biometrics are: {biometric}</Text>
-    </SafeAreaView>
+    </View>
   );
 };
 
