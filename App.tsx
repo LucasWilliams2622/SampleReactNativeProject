469918654ddbab782c9d3a3a3a52f1e82900b7f4
Lucas_Williams <nguyenvanson2622003@gmail.com>
2024-06-07 21:07:29 +0700

    Revert "feat: biometrics"
    This reverts commit b7955d4a188391bc0aced913b5e0fef13bdf439f.

--- App.tsx
+++ App.tsx
@@ -1,76 +1,20 @@
-// import React from 'react';
-// import {Provider} from 'react-redux';
-// import {PersistGate} from 'redux-persist/es/integration/react';
-// import store, {persitor} from './src/redux/store';
-// import {GestureHandlerRootView} from 'react-native-gesture-handler';
-// import Navigator from './src/navigation/Navigator';
-// import {LogBox} from 'react-native';
-// LogBox.ignoreLogs(['Require cycle:']);
-// const App = () => {
-//   return (
-//     <GestureHandlerRootView style={{flex: 1}}>
-//       <Provider store={store}>
-//         <PersistGate loading={null} persistor={persitor}>
-//           <Navigator />
-//         </PersistGate>
-//       </Provider>
-//     </GestureHandlerRootView>
-//   );
-// };
-
-// export default App;
-import React, {useEffect, useState} from 'react';
-import {Button, View, Text} from 'react-native';
-import ReactNativeBiometrics from 'react-native-biometrics';
-
-const rnBiometrics = new ReactNativeBiometrics();
-
+import React from 'react';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/es/integration/react';
+import store, {persitor} from './src/redux/store';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import Navigator from './src/navigation/Navigator';
+import {LogBox} from 'react-native';
+LogBox.ignoreLogs(['Require cycle:']);
 const App = () => {
-  const [biometryType, setBiometryType] = useState(null);
-
-  useEffect(() => {
-    rnBiometrics
-      .isSensorAvailable()
-      .then(resultObject => {
-        const {available, biometryType} = resultObject;
-
-        if (available && biometryType) {
-          setBiometryType(biometryType);
-        } else {
-          setBiometryType(null);
-        }
-      })
-      .catch(() => {
-        setBiometryType(null);
-      });
-  }, []);
-
-  const authenticate = () => {
-    rnBiometrics
-      .simplePrompt({promptMessage: 'Confirm fingerprint'})
-      .then(resultObject => {
-        const {success} = resultObject;
-
-        if (success) {
-          console.log('successful biometrics provided');
-        } else {
-          console.log('user cancelled biometric prompt');
-        }
-      })
-      .catch(() => {
-        console.log('biometrics failed');
-      });
-  };
-
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>
-        {biometryType
-          ? `Biometric Authentication (${biometryType}) available`
-          : 'Biometric Authentication not available'}
-      </Text>
-      {biometryType && <Button title="Authenticate" onPress={authenticate} />}
-    </View>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persitor}>
+          <Navigator />
+        </PersistGate>
+      </Provider>
+    </GestureHandlerRootView>
   );
 };
 
